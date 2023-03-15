3c5ad32546a806c81a6e33c79b604ecdb14611c6
Kevin Vavelin <vavelinkevin@gmail.com>
2023-03-15 20:24:43 +0100

    Single payment using Razor Pages with dependency injection

---
+++ hosted-payment-examples/with-dependency-injection/razor-pages/web/Pages/noParameters/SinglePayment.cshtml.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+using System.Xml.Linq;
+
+namespace web.Pages.noParameters
+{
+    public class SinglePaymentModel : PageModel
+    {
+        [BindProperty]
+        [Display(Name = "Site id", Prompt = "12345678")]
+        [Required(ErrorMessage = "A site id is required")]
+        [MaxLength(8, ErrorMessage = "This site id is too long. A site id is only 8 digits long")]
+        public string SiteId { get; set; } = string.Empty;
+
+        [BindProperty]
+        [Display(Name = "Amount", Prompt = "4525 for EUR 45.25")]
+        [Required(ErrorMessage = "An amount is required")]
+        public string Amount { get; set; } = string.Empty;
+
+        [BindProperty]
+        [Display(Name = "Payment url", Prompt = "https://sogecommerce.societegenerale.eu/vads-payment/")]
+        [Required(ErrorMessage = "A payment url is required")]
+        public string PaymentUrl { get; set; } = string.Empty;
+
+        [BindProperty]
+        [Display(Name = "Api key")]
+        [Required(ErrorMessage = "A development api key is required")]
+        public string ApiKey { get; set; } = string.Empty;
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToPage("/noParameters/MakeSinglePayment", new { siteId = SiteId, apiKey = ApiKey, amount = Amount, paymentUrl = PaymentUrl });
+            }
+            return Page();
+        }
+    }
+}
